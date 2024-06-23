13c42bd41537ef27c9e82cefc2da088a4a90c1f6
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-06-22 21:03:46 -0300

    Maquetado navbar sin funcionalidad [Front]

--- front/src/components/Navbar/GuestNavbar.jsx
+++ front/src/components/Navbar/GuestNavbar.jsx
@@ -16,36 +16,47 @@ const GuestNavbar = () => {
     }, [location]);
 
     return (
-        <header>
+        <header className="navbar">
 
-            <Link to="/">
-                <img src="/collabs-logo.svg" alt="Collabs" />
-            </Link>
+            <div className="container navbar-container">
+                <Link to="/">
+                    <img src="assets/svg/collabs-logo.svg" alt="Collabs" />
+                </Link>
 
-            <nav>
-                <ul>
-                    <li className="nav-item">
-                        <Link to="#que-es">Qué es</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="#como-funciona">Cómo funciona</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="#funcionalidades">Funcionalidades</Link>
-                    </li>
-                    <li className="nav-item">
-                        <p>Explorar</p>
-                        <ul>
-                            <li>
-                                <Link to="#funcionalidades">Proyectos</Link>
-                            </li>
-                            <li>
-                                <Link to="#funcionalidades">Colaboradores</Link>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
+                <button className="mobile-nav-toggle" id="nav-toggle" aria-controls="primary-navigation" aria-expanded="false"></button>
+
+                <nav>
+                    <ul id="primary-navigation" className="primary-navigation">
+                        <li>
+                            <Link to="#que-es" className="navbar-text">Qué es</Link>
+                        </li>
+                        <li>
+                            <Link to="#como-funciona" className="navbar-text">Cómo funciona</Link>
+                        </li>
+                        <li>
+                            <Link to="#funcionalidades" className="navbar-text">Funcionalidades</Link>
+                        </li>
+                        <li>
+                            <div className="nav-link-with-icon">
+                                <p className="navbar-text">Explorar</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 48 48" fill="none">
+                                    <path stroke="#3E4B62" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2.5" d="m33 19-7.41 9.186c-.874 1.085-2.306 1.085-3.18 0L15 19"></path>
+                                </svg>
+                            </div>
+                            <ul className="display-none">
+                                <li>
+                                    <Link to="#funcionalidades">Proyectos</Link>
+                                </li>
+                                <li>
+                                    <Link to="#funcionalidades">Colaboradores</Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+
+                <button className="button-small">Crear cuenta</button>
+            </div>
 
         </header>
     )
