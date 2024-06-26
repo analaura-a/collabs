24ea01ffcfd2a1c6d93100dc4359ed01d2abe562
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-06-26 17:13:01 -0300

    Importar SVGs como componentes de react [Front]

--- front/src/pages/LandingPage/LandingPage.jsx
+++ front/src/pages/LandingPage/LandingPage.jsx
@@ -1,22 +1,9 @@
-// import { ReactComponent as ArrowIcon } from '../../assets/svg/arrow-right.svg';
+import ArrowIcon from '../../assets/svg/arrow-right.svg?react';
 
 const LandingPage = () => {
 
     return (
         <main>
-            {/* <h1>Landing Page</h1>
-
-            <section id="que-es">
-                <h1>Qué es</h1>
-            </section>
-
-            <section id="como-funciona">
-                <h1>Cómo funciona</h1>
-            </section>
-
-            <section id="funcionalidades">
-                <h1>Funcionalidades</h1>
-            </section> */}
 
             <section className="container hero-section">
 
@@ -33,7 +20,7 @@ const LandingPage = () => {
 
                     <div className="hero-section__buttons">
                         <button className="btn btn-secondary btn-medium btn-fitcontent">Cómo funciona</button>
-                        <button className="btn btn-primary btn-medium btn-fitcontent">Empezar a colaborar <span>A</span></button>
+                        <button className="btn btn-primary btn-medium btn-fitcontent">Empezar a colaborar <span><ArrowIcon /></span></button>
                     </div>
 
                 </div>
@@ -43,6 +30,20 @@ const LandingPage = () => {
                 </div>
 
             </section>
+
+            {/* <h1>Landing Page</h1>
+
+            <section id="que-es">
+                <h1>Qué es</h1>
+            </section>
+
+            <section id="como-funciona">
+                <h1>Cómo funciona</h1>
+            </section>
+
+            <section id="funcionalidades">
+                <h1>Funcionalidades</h1>
+            </section> */}
 
         </main>
     )
