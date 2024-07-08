b54c5020b67da62f6aeb655e92685ba234bd0cb2
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-07-08 13:43:57 -0300

    Mostrar dinámicamente el navbar y footer correspondiente [Front]

--- front/src/layouts/MainLayout.jsx
+++ front/src/layouts/MainLayout.jsx
@@ -1,20 +1,25 @@
+import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import GuestNavbar from '../components/Navbar/GuestNavbar';
 import AuthNavbar from '../components/Navbar/AuthNavbar';
 import GuestFooter from '../components/Footer/GuestFooter';
 import AuthFooter from '../components/Footer/AuthFooter';
+import AuthContext from '../context/AuthContext';
 
 const MainLayout = () => {
 
+    const { authState } = useContext(AuthContext);
+    const { token, user } = authState;
+
     return (
         <>
-            <GuestNavbar />
-            {/* <AuthNavbar /> */}
+
+            {token && user ? <AuthNavbar /> : <GuestNavbar />}
 
             <Outlet />
 
-            <GuestFooter />
-            {/* <AuthFooter /> */}
+            {token && user ? <AuthFooter /> : <GuestFooter />}
+
         </>
     )
 }
