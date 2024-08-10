8af96e64800600e0f478278f7699e427f866287d
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-08-09 22:41:30 -0300

    OnboardingCompletePage [Front]

--- front/src/pages/Onboarding/OnboardingCompletePage.jsx
+++ front/src/pages/Onboarding/OnboardingCompletePage.jsx
@@ -1,3 +1,5 @@
+import Button from "../../components/Button/Button";
+
 const OnboardingCompletePage = () => {
 
     return (
@@ -6,8 +8,23 @@ const OnboardingCompletePage = () => {
             <div className="faded-pattern faded-pattern-top-left"></div>
             <div className="faded-pattern faded-pattern-bottom-right"></div>
 
-            <section >
-                ¡Onboarding completado con éxito!
+            <section className="onboarding-page-container onboarding-complete-container">
+
+                <img src='../assets/svg/collabs-logo.svg' alt="Collabs" />
+
+                <div className="onboarding-complete-content">
+
+                    <div className="onboarding-page-container__title">
+                        <h1 className="title-40">¡Todo listo! Ya puedes empezar a colaborar</h1>
+                        <p className="subtitle">En Collabs podrás trabajar junto a otros, adquirir habilidades, hacer conexiones y dar el próximo paso en tu carrera profesional.</p>
+                    </div>
+
+                    <Button size="large" width="full-then-fit">Comenzar</Button>
+
+                </div>
+
+                <img src="../../assets/svg/collaborators-in-line-facing-right.svg" alt="Grupo de personas colaborando" className="onboarding-complete__image" />
+
             </section>
 
         </main>
