a3cd2b89c991e5966bc903fbaf673a75c9af0756
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-08-30 15:59:18 -0300

    Maquetado header de las páginas de explorar [Front]

--- front/src/pages/ExplorePage/ExploreProjectsPage.jsx
+++ front/src/pages/ExplorePage/ExploreProjectsPage.jsx
@@ -1,8 +1,27 @@
+import { Link } from 'react-router-dom';
+
 const ExploreProjectsPage = () => {
 
     return (
         <main>
-            <h1>Proyectos</h1>
+            <div className="container explore-page-container">
+
+                <section className="explore-page__header">
+
+                    <div className="explore-page__header___toggle light-paragraph">
+                        <Link to="/explorar/proyectos" className="toggle-active">Proyectos</Link>
+                        <Link to="/explorar/colaboradores">Colaboradores</Link>
+                    </div>
+
+                    <div>
+                        <h1 className="title-56">Descubre oportunidades de colaboración</h1>
+
+                        {/* Componente de búsqueda */}
+                    </div>
+
+                </section>
+
+            </div>
         </main>
     )
 }
--- front/src/pages/ExplorePage/ExploreUsersPage.jsx
+++ front/src/pages/ExplorePage/ExploreUsersPage.jsx
@@ -1,8 +1,27 @@
+import { Link } from 'react-router-dom';
+
 const ExploreUsersPage = () => {
 
     return (
         <main>
-            <h1>Colaboradores</h1>
+            <div className="container explore-page-container">
+
+                <section className="explore-page__header">
+
+                    <div className="explore-page__header___toggle light-paragraph">
+                        <Link to="/explorar/proyectos">Proyectos</Link>
+                        <Link to="/explorar/colaboradores" className="toggle-active">Colaboradores</Link>
+                    </div>
+
+                    <div>
+                        <h1 className="title-56">Descubre oportunidades de colaboración</h1>
+
+                        {/* Componente de búsqueda */}
+                    </div>
+
+                </section>
+
+            </div>
         </main>
     )
 }
