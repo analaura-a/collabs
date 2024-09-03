50042f77e5a483edc7ddab0ac003ff48aa4c0325
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-03 16:08:09 -0300

    ExploreUsersPage: buscar por palabra clave [Front]

--- front/src/pages/ExplorePage/ExploreUsersPage.jsx
+++ front/src/pages/ExplorePage/ExploreUsersPage.jsx
@@ -1,22 +1,21 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import UserCard from '../../components/Cards/UserCard';
+import SearchAndFilters from '../../components/Inputs/SearchAndFilters';
 import { getUsers } from '../../services/userService';
 
 const ExploreUsersPage = () => {
 
     const [users, setUsers] = useState([]);
-    // const [filteredUsers, setFilteredUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
 
     const [loading, setLoading] = useState(true);
 
-    const fetchUsers = () => {
+    const fetchUsers = async () => {
         try {
-            getUsers()
-                .then((users) => {
-                    setUsers(users)
-                });
-
+            const fetchedUsers = await getUsers();
+            setUsers(fetchedUsers);
+            setFilteredUsers(fetchedUsers);
             setLoading(false);
         } catch (error) {
             console.error('Error al obtener a los usuarios: ', error);
@@ -25,9 +24,22 @@ const ExploreUsersPage = () => {
     };
 
     useEffect(() => {
-        // setFilteredCollaborators(users);
         fetchUsers();
     }, []);
+
+    // Buscar usuarios por `name`, `last_name`, o `username`
+    const handleSearch = (searchTerm) => {
+        if (searchTerm.trim() === "") {
+            setFilteredUsers(users);
+        } else {
+            const matchedUsers = users.filter(user =>
+                user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                user.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                user.username.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            setFilteredUsers(matchedUsers);
+        }
+    };
 
     if (loading) return <div>Cargando colaboradores...</div>; //Componente de carga
 
@@ -42,19 +54,27 @@ const ExploreUsersPage = () => {
                         <Link to="/explorar/colaboradores" className="toggle-active">Colaboradores</Link>
                     </div>
 
-                    <div>
+                    <div className="explore-page__header__title-and-filters">
                         <h1 className="title-56">Encuentra colaboradores para tus proyectos</h1>
 
-                        {/* Componente de búsqueda */}
+                        <SearchAndFilters
+                            placeholder="Buscar personas..."
+                            onSearch={handleSearch} />
                     </div>
 
                 </section>
 
                 <section className="explore-page__container-user-cards">
 
-                    {users.map(user => (
-                        <UserCard key={user._id} user={user} />
-                    ))}
+                    {filteredUsers.length > 0 ? (
+                        filteredUsers.map(user => (
+                            <UserCard key={user._id} user={user} />
+                        ))
+                    ) : (
+                        <div>
+                            No se encontraron colaboradores con esos términos. {/* Maquetar */}
+                        </div>
+                    )}
 
                 </section>
 
