ad2f0d98a73deb63ef2328f3910965e52d777e6f
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-04 16:59:26 -0300

    SearchAndFilters: maquetado de los filtros [Front]

--- front/src/components/Inputs/SearchAndFilters.jsx
+++ front/src/components/Inputs/SearchAndFilters.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 const SearchAndFilters = ({ placeholder, onSearch }) => {
 
     const [searchTerm, setSearchTerm] = useState('');
+    const [activeFilter, setActiveFilter] = useState(null);
 
     useEffect(() => {
         onSearch(searchTerm); // Llamamos a la función de búsqueda cada vez que el término cambia
@@ -13,12 +14,14 @@ const SearchAndFilters = ({ placeholder, onSearch }) => {
         setSearchTerm(event.target.value);
     };
 
-    return (
+    const toggleFilter = (filterName) => {
+        setActiveFilter(activeFilter === filterName ? null : filterName);
+    };
 
+    return (
         <form className="search-and-filters" onSubmit={(e) => e.preventDefault()}>
 
             <label htmlFor="search-input" className="visually-hidden">Buscar</label>
-
             <input
                 type="search"
                 id="search-input"
@@ -28,55 +31,78 @@ const SearchAndFilters = ({ placeholder, onSearch }) => {
                 onChange={handleSearchChange}
                 aria-label="Buscar"
             />
-        </form>
 
-        // <div className="search-and-filters">
+            <ul className="search-and-filters__filters-toggle-container">
 
-        //     <input
-        //         type="text"
-        //         placeholder="Buscar..."
-        //         value={searchTerm}
-        //         onChange={handleSearchChange}
-        //     />
+                <li className="search-and-filters__filter">
+                    <button className={`search-and-filters__filter-toggle smaller-paragraph bold-text ${activeFilter === 'role' ? 'search-and-filters__filter-toggle-active' : ''}`} onClick={() => toggleFilter('role')}>
+                        Rol
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="6" fill="none">
+                            <path stroke="#3B3B3C" strokeLinecap="round" strokeWidth="1.5" d="m1 1 4.146 3.554a.8.8 0 0 0 1.041 0L10.333 1" />
+                        </svg>
+                    </button>
 
-        //     {/* Filtros */}
-        //     <div className="filters">
-        //         <div className="filter-group">
-        //             <label>Rol:</label>
-        //             <select>
-        //                 <option value="">Todos</option>
-        //                 {/* Opciones de roles */}
-        //             </select>
-        //         </div>
+                    {activeFilter === 'role' && (
+                        <div className="search-and-filters__filter-dropdown">
+                            <h2 className="title-18">Rol</h2>
 
-        //         <div className="filter-group">
-        //             <label>Skills:</label>
-        //             <select>
-        //                 <option value="">Todos</option>
-        //                 {/* Opciones de skills */}
-        //             </select>
-        //         </div>
+                            <fieldset className="search-and-filters__checkboxes-container">
 
-        //         <div className="filter-group">
-        //             <label>Disponibilidad:</label>
-        //             <select>
-        //                 <option value="">Todos</option>
-        //                 {/* Opciones de disponibilidad */}
-        //             </select>
-        //         </div>
+                                <div className="search-and-filters__checkbox">
+                                    <input type="checkbox" name="role" value="category1" id='alo' />
+                                    <label htmlFor="alo" className="subtitle">UX/UI Designer</label>
+                                </div>
 
-        //         {/* {filters.experience_level !== undefined && (
-        //             <div className="filter-group">
-        //                 <label>Nivel de experiencia:</label>
-        //                 <select onChange={(e) => handleFilterChange('experience_level', e.target.value)}>
-        //                     <option value="">Todos</option>
 
-        //                 </select>
-        //             </div>
-        //         )} */}
-        //     </div>
+                            </fieldset>
+                        </div>
+                    )}
+                </li>
 
-        // </div>
+                <li className="search-and-filters__filter">
+                    <button className={`search-and-filters__filter-toggle smaller-paragraph bold-text ${activeFilter === 'skills' ? 'search-and-filters__filter-toggle-active' : ''}`} onClick={() => toggleFilter('skills')}>
+                        Skills
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="6" fill="none">
+                            <path stroke="#3B3B3C" strokeLinecap="round" strokeWidth="1.5" d="m1 1 4.146 3.554a.8.8 0 0 0 1.041 0L10.333 1" />
+                        </svg>
+                    </button>
+
+                    {activeFilter === 'skills' && (
+                        <div className="search-and-filters__filter-dropdown">
+                            <h2 className="title-18">Skills</h2>
+
+                        </div>
+                    )}
+                </li>
+
+                <li className="search-and-filters__filter">
+                    <button className={`search-and-filters__filter-toggle smaller-paragraph bold-text ${activeFilter === 'availability' ? 'search-and-filters__filter-toggle-active' : ''}`} onClick={() => toggleFilter('availability')}>
+                        Disponibilidad
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="6" fill="none">
+                            <path stroke="#3B3B3C" strokeLinecap="round" strokeWidth="1.5" d="m1 1 4.146 3.554a.8.8 0 0 0 1.041 0L10.333 1" />
+                        </svg>
+                    </button>
+
+                    {activeFilter === 'availability' && (
+                        <div className="search-and-filters__filter-dropdown">
+                            <h2 className="title-18">Disponibilidad</h2>
+
+                            <fieldset className="search-and-filters__checkboxes-container">
+
+                                <div className="search-and-filters__checkbox">
+                                    <input type="checkbox" name="availability" value="category1" id='alo2' />
+                                    <label htmlFor="alo2" className="subtitle">Media (3-4 horas/día)</label>
+                                </div>
+
+
+                            </fieldset>
+                        </div>
+                    )}
+                </li>
+
+            </ul>
+
+        </form >
     );
 };
 
