95b5815397b4ae8484d12d59cd7193f5277e43f2
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-10 21:37:48 -0300

    ProjectApplicationsPage: empty state [Front]

--- front/src/pages/ProjectRequests/ProjectApplicationsPage.jsx
+++ front/src/pages/ProjectRequests/ProjectApplicationsPage.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom';
 import ApplicationsTable from "../../components/Table/ApplicationsTable";
+import Button from "../../components/Button/Button";
 
 const ProjectApplicationsPage = () => {
+
+    const navigate = useNavigate();
 
     return (
         <main>
@@ -13,8 +17,19 @@ const ProjectApplicationsPage = () => {
                         <p className="subtitle-18">Sigue el estado de los proyectos a los que apliques para unirte a colaborar.</p>
                     </div>
 
-                    {/* Tabla */}
                     <ApplicationsTable />
+
+                    {/* Empty state */}
+                    {/* <div className="applications-page__empty-state">
+                        <img src="../../assets/svg/requests-empty-state.svg" alt="Sin postulaciones" />
+
+                        <div>
+                            <h2 className="title-32-medium">Aún no te has postulado a colaborar en ningún proyecto</h2>
+                            <p className="subtitle-18">Cuando hayas solicitado unirte a colaborar en algún proyecto, podrás seguir el estado de tu postulación aquí.</p>
+                        </div>
+
+                        <Button size="large" width="full-then-fit" onClick={() => navigate('/explorar/proyectos')}>Explorar proyectos</Button>
+                    </div> */}
 
                 </section>
 
