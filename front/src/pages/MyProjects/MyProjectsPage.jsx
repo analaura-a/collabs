e82b866fbf1e5561f4eccf0dc128fd736a96f6bd
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-13 12:52:34 -0300

    MyProjectsPage: maquetado del empty state [Front]

--- front/src/pages/MyProjects/MyProjectsPage.jsx
+++ front/src/pages/MyProjects/MyProjectsPage.jsx
@@ -4,6 +4,8 @@ import Button from '../../components/Button/Button';
 
 const MyProjectsPage = () => {
 
+    const navigate = useNavigate();
+
     return (
         <main>
             <div className="container">
@@ -16,6 +18,32 @@ const MyProjectsPage = () => {
                     </div>
 
                 </section>
+
+                {/* <section className="my-projects-page">
+
+                    <div className="my-projects-page__header">
+                        <h1 className="title-48">Mis proyectos</h1>
+                    </div>
+
+
+                    <div className="my-projects-page__empty-state">
+                        <div className="my-projects-page__empty-state__title">
+                            <img src="../../assets/svg/projects-empty-state.svg" alt="Sin postulaciones" />
+
+                            <div>
+                                <h2 className="title-32-medium">Aún no tienes proyectos</h2>
+                                <p className="subtitle-18">Únete a colaborar en proyectos de otras personas o crea tu propia convocatoria y forma un equipo para tu próximo proyecto.</p>
+                            </div>
+                        </div>
+
+                        <div className="my-projects-page__empty-state__actions">
+                            <Button color="secondary" size="large" width="full-then-fit" onClick={() => navigate('/explorar/proyectos')}>Explorar proyectos</Button>
+                            <Button size="large" width="full-then-fit">Crear nueva convocatoria</Button>
+                        </div>
+
+                    </div>
+
+                </section> */}
 
             </div >
         </main >
