891c8edcf4e0769ef4a579cad1209b2ad1d22162
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-13 12:32:24 -0300

    MyProjectsPage: maquetado y estilos generales [Front]

--- front/src/pages/MyProjects/MyProjectsPage.jsx
+++ front/src/pages/MyProjects/MyProjectsPage.jsx
@@ -8,7 +8,14 @@ const MyProjectsPage = () => {
         <main>
             <div className="container">
 
-              
+                <section className="my-projects-page">
+
+                    <div className="my-projects-page__header">
+                        <h1 className="title-48">Mis proyectos</h1>
+                        <Button size="large" width="full-then-fit">Crear nueva convocatoria</Button>
+                    </div>
+
+                </section>
 
             </div >
         </main >
