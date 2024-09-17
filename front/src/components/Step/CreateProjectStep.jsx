031c86e1874ff448059ed1207e62cf11987132a4
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-17 17:04:45 -0300

    Componente CreateProjectStep [front]
    División en componentes pequeños.

---
+++ front/src/components/Step/CreateProjectStep.jsx
@@ -0,0 +1,33 @@
+import Button from '../Button/Button';
+import Stepper from '../Stepper/Stepper';
+
+const CreateProjectStep = ({ step, title, subtitle, form, currentStep, totalSteps, nextStep, prevStep, isNextDisabled, handleComplete }) => {
+
+    return (
+        <>
+            <Stepper currentStep={currentStep} totalSteps={totalSteps} />
+
+            <div className="create-project__step">
+                <div className="create-project__header">
+                    <div className="create-project__header__title">
+                        <p className="create-project__header__step subtitle primary-color-text medium-text">Paso {step}</p>
+                        <h2 className="title-20 medium-text">{title}</h2>
+                    </div>
+
+                    <p className="paragraph-18">{subtitle}</p>
+                </div>
+            </div>
+
+            {form}
+
+            <div className="onboarding-page-buttons">
+                {currentStep > 0 && <Button size="large" color="secondary" width="full-then-fit" onClick={prevStep}>Atrás</Button>}
+                {currentStep < totalSteps - 1 && <Button size="large" width="full-then-fit" onClick={nextStep} disabled={isNextDisabled}>Siguiente</Button>}
+                {currentStep === totalSteps - 1 && <Button size="large" width="full-then-fit" onClick={handleComplete} disabled={isNextDisabled}>Finalizar</Button>}
+            </div>
+        </>
+    )
+
+}
+
+export default CreateProjectStep;
