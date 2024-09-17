0a786399ae1a9dc399597265e0a82a4057289332
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-17 18:02:09 -0300

    Página CreatOpenSourceProjectPage [Front]

--- front/src/pages/CreateProject/CreatOpenSourceProjectPage.jsx
+++ front/src/pages/CreateProject/CreatOpenSourceProjectPage.jsx
@@ -1,7 +1,95 @@
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../context/AuthContext';
+import CreateProjectStep from '../../components/Step/CreateProjectStep';
+import CreateProjectForm1 from '../../components/Form/CreateProject/OpenSource/CreateProjectForm1';
+import CreateProjectForm2 from '../../components/Form/CreateProject/Personal/CreateProjectForm2';
+
+const steps = [
+    {
+        step: "1",
+        title: "Información del proyecto",
+        subtitle: "Añade toda la información del proyecto necesaria para que aquellas personas interesadas en contribuir puedan conocerlo y decidir si participar en él.",
+        form: CreateProjectForm1,
+    },
+    {
+        step: "2",
+        title: "Tu rol en el proyecto",
+        subtitle: "Selecciona el perfil profesional que más se adecúe al rol que ocuparás dentro de este proyecto.",
+        form: CreateProjectForm2,
+    },
+];
+
+
 const CreatOpenSourceProjectPage = () => {
 
+    const [currentStep, setCurrentStep] = useState(0);
+    const [formData, setFormData] = useState({});
+    const [isStepValid, setIsStepValid] = useState(false);
+
+    const CurrentForm = steps[currentStep].form;
+
+    const nextStep = () => {
+        if (isStepValid && currentStep < steps.length - 1) {
+            setCurrentStep(currentStep + 1);
+            setIsStepValid(false); // Reset validation for the next step
+        }
+    };
+
+    const prevStep = () => {
+        if (currentStep > 0) {
+            setCurrentStep(currentStep - 1);
+            setIsStepValid(true); // Assume previous step is valid
+        }
+    };
+
+    const handleFormChange = (step, data) => {
+        setFormData(prevData => ({
+            ...prevData,
+            [step]: data,
+        }));
+    };
+
+    const handleFormValidation = (isValid) => {
+        setIsStepValid(isValid);
+    };
+
+    const flattenFormData = (formData) => {
+        return Object.values(formData).reduce((acc, data) => ({ ...acc, ...data }), {});
+    };
+
+    const handleComplete = async () => {
+        const flattenedData = flattenFormData(formData);
+        try {
+            console.log('Proyecto creado con éxito:', flattenedData);
+        } catch (error) {
+            console.error('Error al crear el proyecto:', error);
+        }
+    };
+
     return (
-        <h1>Proyecto open-source</h1>
+        <main>
+            <div className="container">
+
+                <section className="create-project-page">
+                    <h1 className="title-40 new-project-page__title">Crear convocatoria para un proyecto <span className="primary-color-text">open-source</span></h1>
+
+                    <CreateProjectStep
+                        step={steps[currentStep].step}
+                        title={steps[currentStep].title}
+                        subtitle={steps[currentStep].subtitle}
+                        form={<CurrentForm onChange={data => handleFormChange(currentStep, data)} onValidate={handleFormValidation} initialData={formData[currentStep]} />}
+                        currentStep={currentStep}
+                        totalSteps={steps.length}
+                        nextStep={nextStep}
+                        prevStep={prevStep}
+                        isNextDisabled={!isStepValid}
+                        handleComplete={handleComplete}
+                    />
+                </section>
+
+            </div>
+        </main>
     )
 }
 
