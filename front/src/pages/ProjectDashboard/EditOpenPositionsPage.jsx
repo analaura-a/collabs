dfd15f9687b408c558554cc980ff190f26e06a9b
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-29 17:28:35 -0300

    EditOpenPositionsPage: maquetado y estilos generales [Front]

--- front/src/pages/ProjectDashboard/EditOpenPositionsPage.jsx
+++ front/src/pages/ProjectDashboard/EditOpenPositionsPage.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+
 const EditOpenPositionsPage = () => {
+
+    const { id } = useParams();
 
     return (
         <main>
             <div className="container edit-project-page">
-                <h1>Editar la convocatoria</h1>
+
+                <Link to={`/mis-proyectos/${id}`} className="small-button-with-icon arrow-left"></Link>
+
+                <section className="edit-project-page__content">
+
+                    <div className="edit-project-page__title">
+                        <div className="project-dashboard__header__top__type">
+                            <img src="../../assets/svg/purple-dot-status.svg" alt="Estatus del proyecto" />
+                            <p className="subtitle medium-text primary-color-text">Personal</p>
+                        </div>
+                        <h1 className="title-40">Editar convocatoria para <span className="primary-color-text">Juguetear</span></h1>
+                    </div>
+
+                    <div className="edit-project-page__form">
+                        <form className="create-project__form-with-inputs" noValidate>
+                        </form>
+                    </div>
+
+                </section>
 
             </div>
         </main>
