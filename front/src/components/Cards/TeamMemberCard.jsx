a0debf6eb4f9ec14bdef5c838a9d6bae47093814
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-09-30 20:01:26 -0300

    TeamMemberCard: cambios para incorporar el dropdownButton [Front]

--- front/src/components/Cards/TeamMemberCard.jsx
+++ front/src/components/Cards/TeamMemberCard.jsx
@@ -5,65 +5,70 @@ const TeamMemberCard = ({ member, projectType, projectStatus, userRole }) => {
 
     const { profile_pic, name, last_name, username, bio, location, role, profile } = member;
 
+    //Que no se muestre DropdownButton si es la card del usuario
+
     return (
-        <article className="user-card">
-            <Link to={`/colaboradores/a`} className="user-card__link">
+        <article className="user-card user-card__link">
 
-                <div className="user-card__with-dropdown">
-                    {profile_pic ? (
-                        <div className="user-card__profile-picture">
-                            <img src={`${SERVER_BASE_URL}${profile_pic}`} alt={`Foto de perfil de ${name} ${last_name}.`} />
-                        </div>
-                    ) : (
-                        <div className="user-card__profile-picture">
-                            <img src="../assets/jpg/no-profile-picture.jpg" alt={`Foto de perfil de ${name} ${last_name}.`} />
-                        </div>
-                    )}
+            <div className="user-card__with-dropdown">
+                {profile_pic ? (
+                    <Link to={`/colaboradores/${username}`} className="user-card__profile-picture">
+                        <img src={`${SERVER_BASE_URL}${profile_pic}`} alt={`Foto de perfil de ${name} ${last_name}.`} />
+                    </Link>
+                ) : (
+                    <Link to={`/colaboradores/${username}`} className="user-card__profile-picture">
+                        <img src="../assets/jpg/no-profile-picture.jpg" alt={`Foto de perfil de ${name} ${last_name}.`} />
+                    </Link>
+                )}
 
-                    {userRole === "Organizador" &&
-                        <DropdownButton />
-                    }
-                </div>
-
-                <div className="user-card__user-info">
-                    <div className="user-card__info-and-role">
-                        <div className="user-card__name">
-                            <h3 className="subtitle-18 bold-text">{name} {last_name}</h3>
-                            {location && (
-                                <p className="smaller-paragraph-light">{location}</p>
-                            )}
-                        </div>
-
-                        {projectType === "Personal" ? (
-                            <div className="user-card__user-role">
-                                <img src="../assets/svg/purple-dot-status-darker.svg" alt="Estatus del proyecto" />
-                                <p className="smaller-paragraph medium-text primary-color-text">{role}</p>
-                            </div>
-                        ) : (
-                            <div className="user-card__user-role">
-                                <p className="smaller-paragraph medium-text primary-color-text">@{username}</p>
-                            </div>
-                        )}
-
-                    </div>
-
-                    {bio ? (
-                        <p className="subtitle truncated-description-3">{bio}</p>
-                    ) : (
-                        <p className="subtitle">{name} {last_name} aún no agregó su biografía.</p>
-                    )}
-                </div>
-
-                {projectType === "Personal" &&
-                    <div className="user-card__professional-info">
-                        <h3 className="subtitle bold-text">Rol en el proyecto</h3>
-                        <ul className="user-card__roles">
-                            <li className="smaller-paragraph">{profile}</li>
-                        </ul>
-                    </div>
+                {userRole === "Organizador" &&
+                    <DropdownButton options={[
+                        {
+                            title: 'Echar del proyecto',
+                            onClick: () => console.log("echar del proyecto")
+                        }
+                    ]} />
                 }
+            </div>
 
-            </Link>
+            <div className="user-card__user-info">
+                <div className="user-card__info-and-role">
+                    <div className="user-card__name">
+                        <h3 className="subtitle-18 bold-text">{name} {last_name}</h3>
+                        {location && (
+                            <p className="smaller-paragraph-light">{location}</p>
+                        )}
+                    </div>
+
+                    {projectType === "Personal" ? (
+                        <div className="user-card__user-role">
+                            <img src="../assets/svg/purple-dot-status-darker.svg" alt="Estatus del proyecto" />
+                            <p className="smaller-paragraph medium-text primary-color-text">{role}</p>
+                        </div>
+                    ) : (
+                        <div className="user-card__user-role">
+                            <p className="smaller-paragraph medium-text primary-color-text">@{username}</p>
+                        </div>
+                    )}
+
+                </div>
+
+                {bio ? (
+                    <p className="subtitle truncated-description-3">{bio}</p>
+                ) : (
+                    <p className="subtitle">{name} {last_name} aún no agregó su biografía.</p>
+                )}
+            </div>
+
+            {projectType === "Personal" &&
+                <div className="user-card__professional-info">
+                    <h3 className="subtitle bold-text">Rol en el proyecto</h3>
+                    <ul className="user-card__roles">
+                        <li className="smaller-paragraph">{profile}</li>
+                    </ul>
+                </div>
+            }
+
         </article>
     );
 };
