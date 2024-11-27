19665df94b0e5d20db14e53b3ea6de84d06011ad
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2024-11-27 12:49:43 -0300

    LandingPage: sección de "acerca de" [Front]

--- front/src/pages/LandingPage/LandingPage.jsx
+++ front/src/pages/LandingPage/LandingPage.jsx
@@ -7,13 +7,12 @@ const LandingPage = () => {
         <main className="landing-page">
 
             <section className="container hero-section">
-
                 <div className="hero-section__info">
 
                     <div className="hero-section__heading">
                         <div className="hero-section__title">
                             <h1>Crece profesionalmente</h1>
-                            <h1 className="title-with-faces">junto a <span><img src="assets/svg/faces.svg" alt="" /></span> otros</h1>
+                            <h1 className="title-with-faces">junto a <span><img src="assets/svg/faces.svg" alt="Colaboradores" /></span> otros</h1>
                         </div>
 
                         <p className='big-subtitle'>En Collabs podrás buscar proyectos, unirte a colaborar, ganar experiencia, hacer conexiones y avanzar en tu carrera.</p>
@@ -27,18 +26,48 @@ const LandingPage = () => {
                 </div>
 
                 <div className="hero-section__photo">
-                    <img src="assets/png/collabs-mockup.png" alt="" />
+                    <img src="assets/png/collabs-mockup.png" alt="Pantallas principales de Collabs" />
+                </div>
+            </section>
+
+
+            <section id="que-es" className="container about-section">
+                <div className="about-section__text-column">
+                    <div className="about-section__text-column__title-and-text">
+                        <div className="about-section__text-column__title">
+                            <p className="landing-page-subtitle">Acerca de</p>
+                            <h2 className="title-40">¿Qué es Collabs?</h2>
+                        </div>
+
+                        <div className="about-section__text-column__text">
+                            <p className="paragraph-18">Collabs es una <span className="medium-text">plataforma de difusión y organización de proyectos colaborativos</span>, diseñada específicamente para personas del <span className="primary-color-text medium-text">rubro IT</span>.</p>
+                            <p className="paragraph-18">En este espacio, profesionales de todos los niveles de experiencia pueden reclutar a voluntarios y trabajar en proyectos de todo tipo durante su tiempo libre, con el objetivo de crecer profesionalmente junto a colegas del rubro.</p>
+                        </div>
+                    </div>
+
+                    <div className="about-section__text-column__project-types">
+                        <h3 className="title-20 medium-text">Tipo de proyectos</h3>
+
+                        <div className="about-section__text-column__project-types__cards">
+                            <div>
+                                <img src="../assets/svg/landing-personal-projects.svg" alt="" />
+                                <p className="paragraph-18">Personales</p>
+                            </div>
+
+                            <div>
+                                <img src="../assets/svg/landing-open-source-projects.svg" alt="" />
+                                <p className="paragraph-18">Open-source</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
+                <div className="about-section__image-column">
+                    <img src="../assets/svg/collaborators-circle.svg" alt="Círculo de colaboradores" />
+                </div>
             </section>
 
-            {/* <h1>Landing Page</h1>
-
-            <section id="que-es">
-                <h1>Qué es</h1>
-            </section>
-
-            <section id="como-funciona">
+            {/*   <section id="como-funciona">
                 <h1>Cómo funciona</h1>
             </section>
 
